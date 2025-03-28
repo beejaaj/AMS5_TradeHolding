e4a9ad300f4bdbff7e4ebb5219076a7110815033
Gabriel Sales Dorea <gabriel@jabuticabagames.com.br>
2025-03-28 15:42:35 -0300

    gitignore

--- backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
+++ backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a32948e8ed1726e36523ca05b28ca6a3f6745be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210ccec6a847b975c8e2fd6eeff70b88304ff567")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
