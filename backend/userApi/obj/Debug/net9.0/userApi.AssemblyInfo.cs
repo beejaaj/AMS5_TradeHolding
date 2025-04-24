d516825d500472fcd67066ac8c85fc347dcf032c
Matheus <mathujo453@gmail.com>
2025-04-24 19:35:24 -0300

    update

--- backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
+++ backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e95fe0d17554a8e375ef2c8e577cc358f15e084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a7628745281b5d6511a4388a77e5f46a7cffc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
